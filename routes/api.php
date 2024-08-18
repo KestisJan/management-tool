df9ca83737299e0df5d2d6e4f89d77401dd600b6
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-18 16:25:43 +0300

    Added authentication and user profile routes with appropriate middleware and prefixes in routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
@@ -12,4 +13,13 @@ Route::group([
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'profile'
+], function () {
+    Route::get('/', [UserProfileController::class, 'show'])->name('profile.show');
+    Route::put('/', [UserProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/', [UserProfileController::class, 'destroy'])->name('profile.destroy');
 });
